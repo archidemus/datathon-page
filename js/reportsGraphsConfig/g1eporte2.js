e206f6e5c57e716352da32d8e8d464c736e2aa35
Ignacio Norambuena <inorambuenaa@gmail.com>
2018-05-24 13:44:30 -0400

    added report2

--- js/reportsGraphsConfig/g1eporte2.js
+++ js/reportsGraphsConfig/g1eporte2.js
@@ -1,4 +1,4 @@
-var svg = d3.select("svg"),
+var svg = d3.select("#g1reporte2"),
     margin = {top: 20, right: 20, bottom: 30, left: 40},
     width = +svg.attr("width") - margin.left - margin.right,
     height = +svg.attr("height") - margin.top - margin.bottom;
@@ -9,7 +9,7 @@ var x = d3.scaleBand().rangeRound([0, width]).padding(0.1),
 var g = svg.append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-d3.tsv("data.tsv", function(d) {
+d3.tsv("http://archidemus.me/datathon-page/data/g1reporte2.tsv", function(d) {
   d.frequency = +d.frequency;
   return d;
 }, function(error, data) {
